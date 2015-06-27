a8685a226417b8966ccacdcb100bb0bf2a31d01f
ryutamaki <ryu.mochi@gmail.com>
2015-06-27 09:38:20 -0700

    Add cocos2d class template which include Node initially

---
+++ Cocos2d-x Class.xctemplate/Node/___FILEBASENAME___.cpp
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+using namespace cocos2d
+
+bool ___FILEBASENAMEASIDENTIFIER___::init() {
+    if (!___VARIABLE_cocos2dxSubclass___::init()) {
+        return false;
+    }
+
+    return true;
+}
---
+++ Cocos2d-x Class.xctemplate/Node/___FILEBASENAME___.h
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::___VARIABLE_cocos2dxSubclass___ {
+public:
+    CREATE_FUNC(___FILEBASENAMEASIDENTIFIER___);
+    bool init() override;
+
+protected:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
