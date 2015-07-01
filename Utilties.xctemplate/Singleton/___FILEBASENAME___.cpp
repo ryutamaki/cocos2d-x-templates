1a864ff3edb3da2e88db122ac451bebd58d58113
ryutamaki <ryu.mochi@gmail.com>
2015-07-01 15:28:04 -0700

    Add utility with singleton class

---
+++ Utilties.xctemplate/Singleton/___FILEBASENAME___.cpp
@@ -0,0 +1,29 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+USING_NS_CC;
+
+static ___FILEBASENAMEASIDENTIFIER___* shared___FILEBASENAMEASIDENTIFIER___;
+___FILEBASENAMEASIDENTIFIER___* ___FILEBASENAMEASIDENTIFIER___::getInstance() {
+    if (!shared___FILEBASENAMEASIDENTIFIER___) {
+        shared___FILEBASENAMEASIDENTIFIER___ = new ___FILEBASENAMEASIDENTIFIER___();
+    }
+
+    return shared___FILEBASENAMEASIDENTIFIER___;
+}
+
+#pragma mark - Constructor and Destructor
+
+___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___() {
+}
+
+___FILEBASENAMEASIDENTIFIER___::~___FILEBASENAMEASIDENTIFIER___() {
+}
+
---
+++ Utilties.xctemplate/Singleton/___FILEBASENAME___.h
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+public:
+    static ___FILEBASENAMEASIDENTIFIER___* getInstance();
+
+private:
+    ___FILEBASENAMEASIDENTIFIER___();
+    ~___FILEBASENAMEASIDENTIFIER___();
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
