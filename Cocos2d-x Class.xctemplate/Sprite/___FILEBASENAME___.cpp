fe2fd45aa9b91886e8150ecfd09ac6bf3183406e
ryutamaki <ryu.mochi@gmail.com>
2015-07-02 13:22:28 -0700

    Add Sprite template using "initWithFile()" method for init

---
+++ Cocos2d-x Class.xctemplate/Sprite/___FILEBASENAME___.cpp
@@ -0,0 +1,33 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+USING_NS_CC;
+
+bool ___FILEBASENAMEASIDENTIFIER___::init()
+{
+    if (!___VARIABLE_cocos2dxSubclass___::initWithFile("<#filename#>"))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+#pragma mark - View lifecycle
+
+void ___FILEBASENAMEASIDENTIFIER___::onEnter()
+{
+    ___VARIABLE_cocos2dxSubclass___::onEnter();
+}
+
+void ___FILEBASENAMEASIDENTIFIER___::onExit()
+{
+    ___VARIABLE_cocos2dxSubclass___::onExit();
+}
---
+++ Cocos2d-x Class.xctemplate/Sprite/___FILEBASENAME___.h
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::___VARIABLE_cocos2dxSubclass___
+{
+public:
+    CREATE_FUNC(___FILEBASENAMEASIDENTIFIER___);
+
+private:
+    bool init() override;
+    void onEnter() override;
+    void onExit() override;
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
